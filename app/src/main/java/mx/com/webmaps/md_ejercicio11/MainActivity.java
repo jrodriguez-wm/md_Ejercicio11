54cfeb483ff43c0b6a42b8fa209294ccf5bdff13
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-19 15:14:15 -0500

    Se crea collapsing Toolbar Layout, con un scrollView

--- app/src/main/java/mx/com/webmaps/md_ejercicio11/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio11/MainActivity.java
@@ -2,12 +2,22 @@ package mx.com.webmaps.md_ejercicio11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_id);
+
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
